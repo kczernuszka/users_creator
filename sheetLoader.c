0aa83379eca5ba86045273fce39c2687ba553195
Karol Czernuszka <kczernuszka@o2.pl>
2020-05-22 17:28:48 +0200

    Select active worksheet

--- sheetLoader.c
+++ sheetLoader.c
@@ -1,18 +1,42 @@
+#include <stddef.h>
+
 #include "freexl.h"
 
-const void *handle;
+const void *xls_handler;
+
+struct Worksheet {
+  unsigned int numberOfRows;
+  short unsigned int numberOfColumns;
+};
+
 
 int load_file (const char *file)
 {
-  unsigned int worksheetCount;
+        unsigned int worksheetCount;
 
-  if (freexl_open(file, &handle) != FREEXL_OK) {
-    return -1;
-  }
+        if (freexl_open(file, &xls_handler) != FREEXL_OK)
+                return -1;
 
-  if (freexl_get_info(handle, FREEXL_BIFF_SHEET_COUNT, &worksheetCount) != FREEXL_OK) {
-    return -1;
-  }
+        if (freexl_get_info(xls_handler, FREEXL_BIFF_SHEET_COUNT, &worksheetCount) != FREEXL_OK)
+                return -1;
 
-  return worksheetCount;
+        return worksheetCount;
+}
+
+
+const char* select_worksheet (struct Worksheet dimensions, unsigned int idx)
+{
+        const char* worksheetName;
+
+        if (freexl_select_active_worksheet(xls_handler, idx) != FREEXL_OK)
+                return NULL;
+
+        if (freexl_worksheet_dimensions(xls_handler, &dimensions.numberOfRows, &dimensions.numberOfColumns) !=
+            FREEXL_OK)
+                return NULL;
+
+        if (freexl_get_worksheet_name(xls_handler, idx, &worksheetName) != FREEXL_OK)
+                return NULL;
+
+        return worksheetName;
 }
